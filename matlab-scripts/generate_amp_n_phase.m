85e3466fa0c4fecccd758f71d038307bc99a43ff
yehonatan <yehonatand123@gmail.com>
2025-06-06 11:17:20 +0300

    add file

--- matlab-scripts/generate_amp_n_phase.m
+++ matlab-scripts/generate_amp_n_phase.m
@@ -2,11 +2,10 @@ function [amp, phase] = generate_amp_n_phase(w, sigma, mu, epsilon, r)
     
     sub_function = generate_sub_function(w, epsilon, sigma);
 
-    alpha = sigma * sqrt(mu ./ (2 * epsilon * max(sub_function, 1e-3)));
-    beta = w .* sqrt((mu * epsilon / 2) * max(sub_function, 1e-3));
+    alpha = sigma * sqrt((mu / 2 / epsilon) ./ sub_function);
+    beta = w .* sqrt((mu * epsilon / 2) * sub_function);
 
     phase = -beta .* (2 * r);
-    amp = exp(-alpha *  2 * r);
-%     amp = ones(size(alpha));  % amp is 1
+    amp = exp(-alpha .*  2 * r);
 
 end
